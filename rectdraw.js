45a66c8ba219eaa8b483c08cd912a29758badf30
Barry Silverman <barry@ubuntu-lynx.(none)>
2011-02-01 21:20:42 -0500

    Round Rects and Nudges

--- rectdraw.js
+++ rectdraw.js
@@ -1,6 +1,7 @@
 var frame;
 var rectlayer, mouselayer, hitbuffer;
-var mx, my, moved, drawmode='r';
+var mx1, my1, mx2, my2;
+var moved, drawmode='r';
 var rects = [];
 var linewidth = 8;
 
@@ -36,7 +37,7 @@ function setupLayers(){
 function handleKey(e){
 	var c = e.charCode;
 	c = String.fromCharCode(c);
-	if('rl12345'.indexOf(c)==-1) return;
+	if('rl12345wasd'.indexOf(c)==-1) return;
 	if(c=='l') drawmode='l';
 	else if(c=='r') drawmode='r';
 	else if(c=='1') linewidth=8;
@@ -44,28 +45,49 @@ function handleKey(e){
 	else if(c=='3') linewidth=12;
 	else if(c=='4') linewidth=14;
 	else if(c=='5') linewidth=16;
+	else if(c=='a') mx1--;
+	else if(c=='d') mx1++;
+	else if(c=='w') my1--;
+	else if(c=='s') my1++;
+	if(window.onmousemove) redrawMouseRect();
 }
 
 function mouseDown(e){
 	e.preventDefault();
 	moved=false;	
-	mx = localx(e.clientX);	
-	my = localy(e.clientY);	
+	mx1 = localx(e.clientX);	
+	my1 = localy(e.clientY);
+	if(drawmode=='l') snapToLine();
 	window.onmousemove = function(e){mouseMove(e)};
 	window.onmouseup = function(e){mouseUp(e)};
 }
 
+function snapToLine(){
+	var idx = readHitBuffer(mx1,my1);
+	if(idx<0) return;
+	var r = rects[idx];
+	if(r[0]!='l') return;
+	if(dist(mx1,my1,r[1],r[2])<5){mx1=r[1];my1=r[2];}
+	if(dist(mx1,my1,r[3],r[4])<5){mx1=r[3];my1=r[4];}
+}
+
+
 function mouseMove(e){
-	var x=localx(e.clientX), y=localy(e.clientY);
-	var dx=Math.abs(x-mx), dy=Math.abs(y-my);
-	if((!moved)&&((dx+dy)<10)) return;
+	mx2=localx(e.clientX), my2=localy(e.clientY);
+	var dx=Math.abs(mx2-mx1), dy=Math.abs(my2-my1);
+	if((!moved)&&((dx+dy)<6)) return;
 	moved = true;
+	redrawMouseRect();
+}
+
+function redrawMouseRect(){
 	var ctx = mouselayer.getContext('2d');
 	ctx.clearRect(0,0,10000,10000);
 	ctx.fillStyle = 'rgba(255,255,0,0.7)';
 	ctx.strokeStyle = 'rgba(255,255,0,0.7)';
-	redrawRect(ctx, [drawmode, mx, my, x, y, linewidth]);
+	redrawRect(ctx, [drawmode, mx1, my1, mx2, my2, linewidth]);
 }
+
 
 
 function mouseUp(e){
@@ -76,7 +98,9 @@ function mouseUp(e){
 	ctx.clearRect(0,0,10000,10000);
 	var x = localx(e.clientX);
 	var y = localy(e.clientY);
-	var rect = [drawmode, mx, my, x, y];
+	if(drawmode=='l'&&(Math.abs(x-mx1)<3)) x=mx1;
+	if(drawmode=='l'&&(Math.abs(y-my1)<3)) y=my1;
+	var rect = [drawmode, mx1, my1, x, y];
 	if(drawmode=='l') rect.push(linewidth);
 	rects.push(rect);
 	redraw();
@@ -118,9 +142,9 @@ function redrawHitBuffer(){
 function redrawRect(ctx, r){
 	var type=r[0]; x1=r[1], y1=r[2], x2=r[3], y2=r[4];
 	if(type=='r'){
-		ctx.fillRect(x1, y1, x2-x1, y2-y1);
+		fillRoundRect(ctx, x1, y1, x2, y2, 3);
 	} else if(type=='l'){
-		ctx.lineCap="square";
+		ctx.lineCap="round";
 		ctx.lineWidth =  r[5];
 		ctx.beginPath();
 		ctx.moveTo(x1, y1);
@@ -129,7 +153,23 @@ function redrawRect(ctx, r){
 	}
 }
 
-function hexdigit(n){return '0123456789ABCDEF'.charAt(n);}
+
+function fillRoundRect(ctx, ix1, iy1, ix2, iy2, r){
+	var x1=Math.min(ix1, ix2), y1=Math.min(iy1,iy2);
+	var x2=Math.max(ix1, ix2), y2=Math.max(iy1,iy2);
+	ctx.beginPath();
+	ctx.moveTo(x1+r,y1);
+	ctx.lineTo(x2-r,y1);
+	ctx.quadraticCurveTo(x2,y1,x2,y1+r);
+	ctx.lineTo(x2,y2-r);
+	ctx.quadraticCurveTo(x2,y2,x2-r,y2);
+	ctx.lineTo(x1+r,y2);
+	ctx.quadraticCurveTo(x1,y2,x1,y2-r);
+	ctx.lineTo(x1,y1+r);
+	ctx.quadraticCurveTo(x1,y1,x1+r,y1);
+	ctx.fill();	
+}
+
 
 function readHitBuffer(x,y){
 	var ctx = hitbuffer.getContext('2d');
@@ -152,6 +192,8 @@ function localy(gy){
 	return Math.max(0, Math.min(550, Math.round(ly)));
 }
 
+function hexdigit(n){return '0123456789ABCDEF'.charAt(n);}
+function dist(x1,y1,x2,y2){return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));}
 
 /////////////////////////
 //
